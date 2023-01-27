d8c4f66dd28e60937a2332401a61d6870bea9306
João Vitor da Silva <jaumm.silva.04@gmail.com>
2023-01-27 17:58:42 -0300

    Commit inicial

---
+++ vuejs-api-core/src/components/Plantas.vue
@@ -0,0 +1,139 @@
+<template>
+<section>
+  <div class="tbl-header">
+    <table cellpadding="0" cellspacing="0" border="0">
+      <thead>
+        <tr>
+          <th>Id</th>
+          <th>Nome</th>
+          <th>Preço</th>
+          <th>Descrição</th>
+          <th></th>
+        </tr>
+      </thead>
+    </table>
+  </div>
+  <div class="tbl-content">
+    <table cellpadding="0" cellspacing="0" border="0">
+      <tbody>
+        <tr>
+          <td>{{ planta.id }}</td>
+          <td>{{ planta.nome }} </td>
+          <td>{{ planta.preco }},00</td>
+          <td>{{ planta.descricao }}</td>
+          <td></td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</section>
+</template>
+
+<script>
+  import axios from "axios"
+  export default {
+   props: ["planta"],
+   data() {
+     return {
+       plantas: [],
+     };
+   },
+   created() {
+     this.getPlantas();
+   },
+   methods: {
+    async getPlantas() {
+        axios
+         .get(`https://localhost:44332/api/Plantas/${this.planta.id}/`)
+         .then((res) => {
+           this.plantas = res.data;
+         })
+         .catch((error) => {
+           console.log(error);
+         });
+   },
+   
+  }
+}
+
+  </script>
+
+<style>
+
+table{
+  width:100%;
+  table-layout: fixed;
+}
+.tbl-header{
+  background-color: rgba(255,255,255,0.3);
+ }
+.tbl-content{
+  overflow-x:auto;
+  margin-top: 0px;
+  border: 1px solid rgba(255,255,255,0.3);
+}
+th{
+  padding: 20px 15px;
+  text-align: left;
+  font-weight: 500;
+  font-size: 12px;
+  color: #fff;
+  text-transform: uppercase;
+}
+td{
+  padding: 15px;
+  text-align: left;
+  vertical-align:middle;
+  font-weight: 300;
+  font-size: 12px;
+  color: #fff;
+  border-bottom: solid 1px rgba(255,255,255,0.1);
+}
+
+
+/* demo styles */
+section{
+  margin: 50px;
+}
+
+
+/* follow me template */
+.made-with-love {
+  margin-top: 40px;
+  padding: 10px;
+  clear: left;
+  text-align: center;
+  font-size: 10px;
+  font-family: arial;
+  color: #fff;
+}
+.made-with-love i {
+  font-style: normal;
+  color: #F50057;
+  font-size: 14px;
+  position: relative;
+  top: 2px;
+}
+.made-with-love a {
+  color: #fff;
+  text-decoration: none;
+}
+.made-with-love a:hover {
+  text-decoration: underline;
+}
+
+
+/* for custom scrollbar for webkit browser*/
+
+::-webkit-scrollbar {
+    width: 6px;
+} 
+::-webkit-scrollbar-track {
+    -webkit-box-shadow: inset 0 0 6px rgba(0,0,0,0.3); 
+} 
+::-webkit-scrollbar-thumb {
+    -webkit-box-shadow: inset 0 0 6px rgba(0,0,0,0.3); 
+}
+
+
+</style>
---
+++ vuejs-api-core/src/views/HomeView.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <h1>Plantas</h1>
+    <Plantas 
+    v-for="planta in plantas" :key="planta.id" :planta="planta" />
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+import Plantas from "../components/Plantas.vue";
+export default {
+  name: 'App',
+  components: {Plantas},
+  data() {
+    return {
+      planta: {},
+      plantas: [],
+    };  
+  },
+  async created() {
+    await this.getPlantas();
+  },
+  methods: {
+    async getPlantas() {
+        axios
+         .get(`https://localhost:44332/api/Plantas/`)
+         .then((res) => {
+           this.plantas = res.data;
+         })
+         .catch((error) => {
+           console.log(error);
+         });
+   },
+  }
+}
+</script>
+
+<style>
+.planta {
+  height: 1000px;
+}
+h1{
+  font-size: 30px;
+  color: #fff;
+  text-transform: uppercase;
+  font-weight: 300;
+  text-align: center;
+  margin-bottom: 15px;
+}
+
+body{
+  background: -webkit-linear-gradient(left, #25c481, #25b7c4);
+  background: linear-gradient(to right, #25c481, #25b7c4);
+  font-family: 'Roboto', sans-serif;
+}
+</style>
